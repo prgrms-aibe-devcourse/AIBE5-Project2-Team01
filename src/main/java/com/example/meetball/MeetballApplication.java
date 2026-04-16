6497e2bbb716130550ba1f5f7be9a4fd65f4530b
jychoi0831 <jy.choi0831@gmail.com>
2026-04-16 11:39:50 +0900

    chore: Spring Boot 프로젝트 초기 설정
    Spring Initializr에서 Maven, Spring Boot, Java 버전 등 초기 환경 구성

---
+++ src/main/java/com/example/meetball/MeetballApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetballApplication.class, args);
+	}
+
+}
