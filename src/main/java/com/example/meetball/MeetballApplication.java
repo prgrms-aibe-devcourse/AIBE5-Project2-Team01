84af28b01a73a34924aeaa7fcefb2a03b6621c77
최윤석 <aa01066303541@gmail.com>
2026-04-27 13:56:07 +0900

    댓글 시간 한국 시간으로 수정
    국제 표준시 기준이었대요

--- src/main/java/com/example/meetball/MeetballApplication.java
+++ src/main/java/com/example/meetball/MeetballApplication.java
@@ -2,9 +2,16 @@ package com.example.meetball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class MeetballApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeetballApplication.class, args);
